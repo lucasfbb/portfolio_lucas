956f03f877d09a3d3648684a72f5c1416568d577
Lucas Firmino <lucasfirmino2612@gmail.com>
2024-03-01 15:14:54 -0300

    ......

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,35 @@
 
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+
+    const job = document.getElementById('profile.perfil')
+    job.innerText= profileData.perfil
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const wpp = document.getElementById('profile.wpp')
+    wpp.innerText = profileData.wpp
+    wpp.href = `tel:${profileData.wpp}`
+
+    const email = document.getElementById('profile.msg')
+    email.innerText = profileData.msg
+    email.href = `mailto:${profileData.msg}`
+
+    const github = document.getElementById('profile.git')
+    github.innerText = profileData.git
+
+}
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills')
+
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
 (async () => {
-
     const profileData = await fetchProfileData()
-    console.log(profileData)
-
+    updateProfileInfo(profileData)
 })()
