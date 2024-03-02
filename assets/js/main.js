3c58928dbfe288c09e825f038bcd29d2edcd52c6
Lucas Firmino <lucasfirmino2612@gmail.com>
2024-03-02 00:14:45 -0300

    .................

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,12 +24,51 @@ function updateProfileInfo(profileData) {
 }
 
 function updateSoftSkills(profileData) {
-    const softSkills = document.getElementById('profile.skills.softSkills')
+    const softSkills = document.getElementById('profile.skills.softskills')
 
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardskills')
+
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+}
+
+function updateLanguages(profileData) {
+    const lang = document.getElementById('profile.languages')
+
+    lang.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
+}
+
+function updateProjects(profileData) {
+    const project = document.getElementById('profile.portfolio')
+
+    project.innerHTML = profileData.portfolio.map(project => 
+    `<li>
+    <span class="title github">${project.name}</span>
+    <p class="descricao">${project.description}</p>
+    <a href="${project.url}" target="_blank">github.com/lucasfbb/${project.caminho}</a>
+    </li>`).join('')
+}
+
+function updateExperience(profileData) {
+    const experience = document.getElementById('profile.professionalExperience')
+
+    experience.innerHTML = profileData.professionalExperience.map(experience => 
+    `<li>
+    <span class="title">${experience.name}</span>
+    <span class="subtitle">${experience.period}</span>
+    <p class="paragrafo">${experience.description}</p>
+    </li>`).join('')
 }
 
 (async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
+    updateSoftSkills(profileData)
+    updateHardSkills(profileData)
+    updateLanguages(profileData)
+    updateProjects(profileData)
+    updateExperience(profileData)
 })()
